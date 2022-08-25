e59e756eb3987ef77ad37bb3779f284a50a1e830
m-h-williams <110432901+m-h-williams@users.noreply.github.com>
2022-08-25 14:12:05 -0700

    GPS to Haversine and Euclidean Distance
    Script to determine the Haversine and Euclidean distance from GPS lat/long coordinates

--- code/GPS_distance.R
+++ code/GPS_distance.R
@@ -10,16 +10,29 @@ setwd("/Users/meganwilliams/Documents/GitHub/Seattle_Aquarium_ROV_telemetry_and_
 # bring in cleaned primary telemetry data file
 dat = read.csv("2022-08-15_09-56-00_vehicle1_cleaned.csv")
 
+## Haversine Distance ------------------------
 # create new data frame with Distance column. Distance is the difference in lon, lat 
     #between rows converted from degrees to meters using the 'Haversine method'
-dat2 <- dat %>% mutate(dat, Distance = distHaversine(cbind(lon, lat), cbind(lag(lon), 
+dat2 <- dat %>% mutate(dat, HavDIS = distHaversine(cbind(lon, lat), cbind(lag(lon), 
                                       lag(lat))))
 
-# save csv with distance column 
+# Euclidean Distance -----------------------
+library(rgdal)
+
+#Convert coordinates to SpatialPoint object 
+cord.dec = SpatialPoints(cbind(dat$lon, -dat$lat), proj4string = CRS("+proj=longlat"))
+
+# Transform coordinate system to UTM using EPSG:32610 - WGS 84 / UTM zone 10N
+cord.UTM <- spTransform(cord.dec, CRS("+init=epsg:32610"))
+head(cord.UTM)
+
+# Calculate Euclidean distance (m) from UTM coordinates 
+euclidDist <- sp::spDists(cord.UTM, longlat = FALSE, segments=TRUE)
+
+# Append Euclidean distance values to data frame with first row as NA
+dat2$EucDIS <- c(NA, euclidDist)
+
+# save csv with Haversine and Euclidean distance columns 
 write.csv(dat2, '2022-08-15_09-56-00_vehicle1_cleaned_distance.csv', row.names = FALSE)
 
-
 ## END distance calculation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
