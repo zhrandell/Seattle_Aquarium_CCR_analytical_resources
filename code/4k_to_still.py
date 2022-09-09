0fc6183526f0ba9aca25ecd9e9c47106c104eb35
m-h-williams <110432901+m-h-williams@users.noreply.github.com>
2022-09-09 15:10:26 -0700

    Extract still images from 4k video
    saves image names as date_time_+second.jpg

--- code/4k_to_still.py
+++ code/4k_to_still.py
@@ -1,20 +1,19 @@
 import cv2
 import os
-
 import sys
 import argparse
 
 def extractImages(pathIn, pathOut):
 	count = 0
-	vidcap = cv2.VideoCapture('//Users/meganwilliams/Documents/SEAQ/ROV_video/Centennial_park_trim.mp4')
+	vidcap = cv2.VideoCapture('/Users/meganwilliams/Library/CloudStorage/OneDrive-Personal/Imagery/2022_8_15/vignettes/clip_for_CoralNet.mp4')
 	success,image = vidcap.read()
 	success = True
 	while success:
 		vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) 
 		success,image = vidcap.read()
 		print('Read a new frame: ', success)
-		cv2.imwrite( '/Users/meganwilliams/Documents/SEAQ/ROV_video/Centennial_stills' + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
-		count = count + 10 #every 10 seconds 
+		cv2.imwrite( '/Users/meganwilliams/Library/CloudStorage/OneDrive-Personal/Imagery/2022_08_15/vignettes/2022_08_15_10-01-31_+' + "%d.jpg" % count, image)     # save frame as JPEG file
+		count = count + 30 #every 30 seconds 
 
 	    
 
