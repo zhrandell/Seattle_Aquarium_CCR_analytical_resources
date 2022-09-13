56a4b6a5cb08c111b3c9c686e8c2a4d5de9b365c
m-h-williams <110432901+m-h-williams@users.noreply.github.com>
2022-09-13 08:22:07 -0700

    Merge telemetry and Ping files

--- code/Merge_Telem_Ping.R
+++ code/Merge_Telem_Ping.R
@@ -1,21 +1,18 @@
+setwd(here::here())
+
 #load libraries
 library(tidyverse)
 library(ggplot2)
 
 #set paths and read in main telemetry file and Ping file from same transect 
 
-setwd('/Users/meganwilliams/Documents/GitHub/Seattle_Aquarium_ROV_telemetry_and_mapping/ROV_telemetry/QGC')
-main = read.csv("2022-08-15_09-56-00_vehicle1_cleaned_distance.csv")
-
-
-setwd('/Users/meganwilliams/Documents/GitHub/Seattle_Aquarium_ROV_telemetry_and_mapping/ROV_telemetry/Ping')
-Ping = read.csv("20220815-095728695_Avg_Ping.csv")
+main = read.csv("./ROV_telemetry/QGC/2022-08-15_09-56-00_vehicle1_cleaned_distance.csv")
+Ping = read.csv("./ROV_telemetry/Ping/20220815-095728695_Avg_Ping.csv")
 
 #drop unnecessary X column
 main <- subset(main, select = -X)
 
-#Merge data frames together, drop rows with NA values
-
-dat4 <- merge(main, Ping, all=FALSE)
+#Merge data frames together based on time 
+dat4 <- merge(main, Ping, by.x = "time")
 
 
