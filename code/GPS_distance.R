89f196be248ab07784e1fbf7a69c8ff520e4fa43
m-h-williams <110432901+m-h-williams@users.noreply.github.com>
2022-09-15 15:10:00 -0700

    GPS to Euclidean distance (m)

--- code/GPS_distance.R
+++ code/GPS_distance.R
@@ -1,24 +1,30 @@
 ### Calculate distance between GPS points 
 
+#set working directory 
+setwd(here::here())
+
+
 #load packages 
 library(geosphere)
 library(dplyr)
 library(measurements)
+library(rgdal)
 
-#set path
-setwd("/Users/meganwilliams/Documents/GitHub/Seattle_Aquarium_ROV_telemetry_and_mapping/ROV_telemetry/QGC")
 
-# bring in cleaned primary telemetry data file
-dat = read.csv("2022-08-15_09-56-00_vehicle1_cleaned.csv")
+## bring in cleaned primary telemetry data file
+# path to Ping files = ./ROV_telemetry/Ping
+# path to telemetry files = ./ROV_telemetry/QGC
 
-## Haversine Distance ------------------------
+dat = read.csv("./ROV_telemetry/QGC/2022-08-15_09-56-00_vehicle1_cleaned.csv")
+
+
+## Haversine Distance (not using) ------------------------
 # create new data frame with Distance column. Distance is the difference in lon, lat 
 # between rows converted from degrees to meters using the 'Haversine method'
-dat2 <- dat %>% mutate(dat, HavDIS = distHaversine(cbind(lon, lat), cbind(lag(lon), 
-                                      lag(lat))))
+# dat2 <- dat %>% mutate(dat, HavDIS = distHaversine(cbind(lon, lat), cbind(lag(lon), lag(lat))))
 
-# Euclidean Distance -----------------------
-library(rgdal)
+
+## Euclidean Distance -----------------------
 
 #Convert coordinates to SpatialPoint object 
 cord.dec = SpatialPoints(cbind(dat$lon, -dat$lat), proj4string = CRS("+proj=longlat"))
@@ -31,9 +37,9 @@ head(cord.UTM)
 euclidDist <- sp::spDists(cord.UTM, longlat = FALSE, segments=TRUE)
 
 # Append Euclidean distance values to data frame with first row as NA
-dat2$EucDIS <- c(NA, euclidDist)
+dat$EucDIS <- c(NA, euclidDist)
 
 # save csv with Haversine and Euclidean distance columns 
-write.csv(dat2, '2022-08-15_09-56-00_vehicle1_cleaned_distance.csv', row.names = FALSE)
+write.csv(dat, './ROV_telemetry/QGC/2022-08-15_09-56-00_vehicle1_cleaned_distance.csv', row.names = FALSE)
 
 ## END distance calculation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
