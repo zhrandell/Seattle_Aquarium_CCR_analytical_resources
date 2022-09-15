d26055d1cc9f2f816b8a086d07e576406a04d55b
m-h-williams <110432901+m-h-williams@users.noreply.github.com>
2022-09-15 15:10:24 -0700

    Merge telemetry and Ping files

--- code/Merge_Telem_Ping.R
+++ code/Merge_Telem_Ping.R
@@ -1,10 +1,14 @@
+#set working directory 
 setwd(here::here())
 
 #load libraries
 library(tidyverse)
 library(ggplot2)
 
-#set paths and read in main telemetry file and Ping file from same transect 
+
+## Set paths and read in main telemetry file and Ping file from same transect 
+# path to Ping files = ./ROV_telemetry/Ping
+# path to telemetry files = ./ROV_telemetry/QGC
 
 main = read.csv("./ROV_telemetry/QGC/2022-08-15_09-56-00_vehicle1_cleaned_distance.csv")
 Ping = read.csv("./ROV_telemetry/Ping/20220815-095728695_Avg_Ping.csv")
@@ -13,6 +17,6 @@ Ping = read.csv("./ROV_telemetry/Ping/20220815-095728695_Avg_Ping.csv")
 main <- subset(main, select = -X)
 
 #Merge data frames together based on time 
-dat4 <- merge(main, Ping, by.x = "time")
+main_ping <- merge(main, Ping, by.x = "time")
 
 
